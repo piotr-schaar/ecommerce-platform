1449207bf2f42035354472f5f08e84307b31d1d7
piotr-schaar <piotr.schaar@gmail.com>
2020-01-26 01:41:37 +0100

    cart functionality added

--- src/components/Cart/CartList.js
+++ src/components/Cart/CartList.js
@@ -3,9 +3,23 @@ import { useStore } from 'hoc/Context';
 
 const CartList = () => {
   const { state, dispatch } = useStore();
-  console.log('TCL: CartList -> state', state);
 
-  return <div></div>;
+  return (
+    <div>
+      <ul>
+        {state.items.map(item => (
+          <>
+            <li>{item.name}</li>
+            <li>{item.quantity}</li>
+            <button onClick={() => dispatch({ type: 'ADD_ITEM', payload: item })}>dodaj</button>
+            <button onClick={() => dispatch({ type: 'REMOVE_ITEM', payload: item })}>
+              ODEJMIJ
+            </button>
+          </>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default CartList;
--- src/hoc/Context.js
+++ src/hoc/Context.js
@@ -7,18 +7,50 @@ export const initialState = {
 export const cartReducer = (state, action) => {
   switch (action.type) {
     case 'ADD_ITEM':
-      return { items: [...state.items, action.payload] };
+      const targetedProductById = state.items.filter(item => item.id === action.payload.id);
+      if (targetedProductById.length) {
+        const indexTargetedProduct = state.items.indexOf(targetedProductById[0]);
+        return {
+          ...state,
+          items: state.items.map((content, i) =>
+            i === indexTargetedProduct ? { ...content, quantity: content.quantity + 1 } : content,
+          ),
+        };
+      } else {
+        return {
+          ...state,
+          items: [...state.items, { ...action.payload, quantity: 1 }],
+        };
+      }
+    case 'REMOVE_ITEM': {
+      const indexOfTargetProduct = state.items.indexOf(action.payload);
+      if (state.items[indexOfTargetProduct].quantity === 1) {
+        const arrWithRemovedProduct = state.items.filter(
+          item => item != state.items[indexOfTargetProduct],
+        );
+        return {
+          ...state,
+          items: arrWithRemovedProduct,
+        };
+      }
+      return {
+        ...state,
+        items: state.items.map((content, i) =>
+          i === indexOfTargetProduct ? { ...content, quantity: content.quantity - 1 } : content,
+        ),
+      };
+    }
     default:
       return state;
   }
 };
 const StoreContext = createContext(null);
 
-export function StoreProvider({ children }) {
+export const StoreProvider = ({ children }) => {
   const [state, dispatch] = useReducer(cartReducer, initialState);
   const value = { state, dispatch };
 
   return <StoreContext.Provider value={value}>{children}</StoreContext.Provider>;
-}
+};
 
 export const useStore = () => useContext(StoreContext);
