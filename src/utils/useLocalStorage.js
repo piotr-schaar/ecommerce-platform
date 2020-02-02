151fbdac71dda2130780fbb679cdeb5eb8fc07d5
piotr schaar <piotr.schaar@gmail.com>
2020-02-02 19:01:31 +0100

    uneeded imports removed

--- src/utils/useLocalStorage.js
+++ src/utils/useLocalStorage.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 const useLocalStorage = localStorageKey => {
   const [value, setValue] = useState(localStorage.getItem(localStorageKey) || null);
